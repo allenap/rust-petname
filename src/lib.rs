403f625b64dfc23f1fe4369022b683f0aceaa187
Gavin Panella <gavin@gromper.net>
2017-06-02 23:28:13 +0200

    Change Petnamer to Petnames: don't hold onto the RNG.

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,42 @@
 
 extern crate rand;
 
-use self::rand::Rng;
+
+pub fn petname(words: u16, separator: &str) -> String {
+    let petnames = Petnames::default();
+    let mut rng = rand::thread_rng();
+    generate(&petnames, &mut rng, words, separator)
+}
 
 
-pub struct Petnamer<'a> {
+pub fn generate<RNG>(
+    petnames: &Petnames, rng: &mut RNG,
+    words: u16, separator: &str) -> String
+    where RNG: rand::Rng
+{
+    // Adverbs all the way, finishing with adjective then name.
+    let mut parts = Vec::with_capacity(words as usize);
+    for num in (0..words).rev() {
+        parts.push(*match num {
+            0 => rng.choose(&petnames.names).unwrap(),
+            1 => rng.choose(&petnames.adjectives).unwrap(),
+            _ => rng.choose(&petnames.adverbs).unwrap(),
+        });
+    };
+    // Join the string parts.
+    parts.join(separator)
+}
+
+
+pub struct Petnames<'a> {
     pub adjectives: Vec<&'a str>,
     pub adverbs: Vec<&'a str>,
     pub names: Vec<&'a str>,
-    pub rng: Box<Rng>,
 }
 
-impl<'a> Petnamer<'a> {
+impl<'a> Petnames<'a> {
 
-    pub fn new() -> Petnamer<'a> {
+    pub fn default() -> Petnames<'a> {
         let adjectives = concat!(
             include_str!("../words/large/adjectives.txt"), "\n",
             include_str!("../words/medium/adjectives.txt"), "\n",
@@ -30,36 +53,14 @@ impl<'a> Petnamer<'a> {
             include_str!("../words/medium/names.txt"), "\n",
             include_str!("../words/small/names.txt"), "\n",
         );
-        Petnamer{
+        Self{
             adjectives: adjectives.split_whitespace().collect(),
             adverbs: adverbs.split_whitespace().collect(),
             names: names.split_whitespace().collect(),
-            rng: Box::new(rand::thread_rng()),
         }
     }
 
-    pub fn generate(&mut self, words: u16, separator: &str) -> String {
-        // Adverbs all the way, finishing with adjective then name.
-        let mut parts = Vec::with_capacity(words as usize);
-        let ref mut rng = self.rng;
-        for num in (0..words).rev() {
-            parts.push(*match num {
-                0 => rng.choose(&self.names).unwrap(),
-                1 => rng.choose(&self.adjectives).unwrap(),
-                _ => rng.choose(&self.adverbs).unwrap(),
-            });
-        };
-        // Join the string parts.
-        parts.join(separator)
-    }
-
 }
-
-
-pub fn petname(words: u16, separator: &str) -> String {
-    Petnamer::new().generate(words, separator)
-}
-
 
 
 #[cfg(test)]
