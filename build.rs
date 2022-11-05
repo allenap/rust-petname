31f68d317cb6541a2b0f25a791427cd8382147a3
Gavin Panella <gavinpanella@gmail.com>
2022-11-05 11:34:20 +0100

    Organise into modules

--- build.rs
+++ build.rs
@@ -6,29 +6,27 @@ fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("words.rs");
 
-    let list_paths = [
-        "words/small/adjectives.txt",
-        "words/small/adverbs.txt",
-        "words/small/names.txt",
-        "words/medium/adjectives.txt",
-        "words/medium/adverbs.txt",
-        "words/medium/names.txt",
-        "words/large/adjectives.txt",
-        "words/large/adverbs.txt",
-        "words/large/names.txt",
-    ];
-
     let mut lines: Vec<String> = vec![];
 
-    for list_path in list_paths {
-        println!("cargo:rerun-if-changed={list_path}");
-        let list_raw = fs::read_to_string(list_path).unwrap();
-        let list = list_raw.split_whitespace().collect::<Vec<_>>();
-        let list_path_parts = list_path.split(&['/', '.']).collect::<Vec<_>>();
-        let [_, list_size, list_name, _] = list_path_parts[..] else { panic!() };
-        lines.push(format!("const {list_name}_{list_size}: [&str; _] = ["));
-        lines.extend(list.iter().map(|word| format!("    \"{word}\",")));
-        lines.push("];".into());
+    let list_sizes = ["small", "medium", "large"];
+    let list_names = ["adjectives", "adverbs", "names"];
+
+    for list_size in list_sizes {
+        lines.push(format!("pub mod {list_size} {{"));
+        for list_name in list_names {
+            let list_path = format!("words/{list_size}/{list_name}.txt");
+            println!("cargo:rerun-if-changed={list_path}");
+            let list_raw = fs::read_to_string(list_path).unwrap();
+            let list = list_raw.split_whitespace().collect::<Vec<_>>();
+            lines.push(format!(
+                "  pub const {}: [&str; {}] = [",
+                list_name.to_uppercase(),
+                list.len()
+            ));
+            lines.extend(list.iter().map(|word| format!("    \"{word}\",")));
+            lines.push(format!("  ];"));
+        }
+        lines.push(format!("}}"));
     }
 
     fs::write(&dest_path, lines.join("\n")).unwrap();
