82520e6e76355e56ee6d27e2fd37357d224d702e
Gavin Panella <gavin@allenap.me>
2023-12-02 09:25:33 +0100

    Build `words.rs` hygienically

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 use anyhow::{Context, Result};
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
 use std::collections::HashSet;
 use std::env;
 use std::fs;
@@ -9,36 +11,57 @@ fn main() -> Result<()> {
     let out_dir = env::var_os("OUT_DIR").context("OUT_DIR not set")?;
     let dest_path = Path::new(&out_dir).join("words.rs");
 
-    let mut lines: Vec<String> = vec![];
-
     let list_sizes = ["small", "medium", "large"];
     let list_names = ["adjectives", "adverbs", "nouns"];
 
-    for list_size in list_sizes {
-        lines.push(format!("pub mod {list_size} {{"));
-        for list_name in list_names {
-            let list_path = words_dir.join(list_size).join(list_name).with_extension("txt");
-            println!("cargo:rerun-if-changed={}", list_path.to_string_lossy());
-            let list_raw = fs::read_to_string(&list_path)
-                .with_context(|| format!("Could not read word list from {list_path:?}"))?;
-            let list = {
-                // Ensure we have no duplicates.
-                let words = list_raw.split_whitespace().collect::<HashSet<_>>();
-                // Collect into a `Vec` and sort it.
-                let mut list = words.into_iter().collect::<Vec<_>>();
-                list.sort();
-                list
-            };
-            lines.push(format!("  pub static {}: [&str; {}] = [", list_name.to_uppercase(), list.len()));
-            lines.extend(list.iter().map(|word| format!("    \"{word}\",")));
-            lines.push("  ];".to_string());
-        }
-        lines.push("}".to_string());
-    }
+    // Generate modules for each list size.
+    let list_modules = list_sizes
+        .into_iter()
+        .map(|list_size| {
+            // Generate static lists for each list name.
+            let lists = list_names
+                .into_iter()
+                .map(|list_name| {
+                    let words_path = words_dir.join(list_size).join(list_name).with_extension("txt");
+                    // Remind Cargo to re-run if the word list changes.
+                    println!("cargo:rerun-if-changed={}", words_path.to_string_lossy());
+                    let words_raw = fs::read_to_string(&words_path)
+                        .with_context(|| format!("Could not read word list from {words_path:?}"))?;
+                    let words = split_words_deduplicate_and_sort(&words_raw);
+                    let list_ident = format_ident!("{}", list_name.to_uppercase());
+                    let list_len = words.len();
+                    let list = quote! { pub static #list_ident: [&str; #list_len] = [ #( #words ),* ]; };
+                    Ok(list)
+                })
+                .collect::<Result<Vec<TokenStream>>>()?;
+            let module_ident = format_ident!("{}", list_size);
+            let module_tokens = quote! { pub mod #module_ident { #(#lists)* } };
+            Ok(module_tokens)
+        })
+        .collect::<Result<Vec<TokenStream>>>()?;
 
-    fs::write(&dest_path, lines.join("\n"))
+    // Assemble the complete `words` module.
+    let modules = quote! { #(#list_modules)* };
+    let module = modules.to_string();
+
+    // Write the module to the output file.
+    fs::write(&dest_path, module)
         .with_context(|| format!("Could not write word lists to output file {dest_path:?}"))?;
+
+    // DEBUG: Print the generated file's path as a warning:
+    // println!("cargo:warning={}", dest_path.display());
+
+    // Remind Cargo when to re-run this build script.
     println!("cargo:rerun-if-changed=build.rs");
 
     Ok(())
+}
+
+fn split_words_deduplicate_and_sort(input: &str) -> Vec<&str> {
+    // Ensure we have no duplicates.
+    let words = input.split_whitespace().collect::<HashSet<_>>();
+    // Collect into a `Vec` and sort it.
+    let mut words = words.into_iter().collect::<Vec<_>>();
+    words.sort();
+    words
 }
